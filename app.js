206d1e84e39b9128f6cfb826f20fa3593922ace5
Mushfiq Rashid <mushfiqarashid@gmail.com>
2026-05-07 20:12:16 -0700

    finish express app

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const routes = require('./routes/index.js');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', routes);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+module.exports = app;
